b9300c914099d776bb0ec5e8598a6ecd24fa5ad7
pengfei.zhou <pengfeizhou@foxmail.com>
2020-09-17 20:01:41 +0800

    Add SQLite plugin

--- android/sqlite/src/main/java/pub/doric/extension/sqlite/DoricSQLitePlugin.java
+++ android/sqlite/src/main/java/pub/doric/extension/sqlite/DoricSQLitePlugin.java
@@ -15,7 +15,15 @@
  */
 package pub.doric.extension.sqlite;
 
+import com.github.pengfeizhou.jscore.JSArray;
 import com.github.pengfeizhou.jscore.JSObject;
+import com.github.pengfeizhou.jscore.JavaValue;
+
+import org.json.JSONArray;
+
+import java.io.File;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import pub.doric.DoricContext;
 import pub.doric.extension.bridge.DoricMethod;
@@ -30,12 +38,66 @@ import pub.doric.plugin.DoricJavaPlugin;
  */
 @DoricPlugin(name = "sqlite")
 public class DoricSQLitePlugin extends DoricJavaPlugin {
+    private ConcurrentHashMap<String, DoricSQLiteDatabase> sqLiteDatabaseMap = new ConcurrentHashMap<>();
+    private AtomicInteger atomicInteger = new AtomicInteger(0);
+
     public DoricSQLitePlugin(DoricContext doricContext) {
         super(doricContext);
     }
 
     @DoricMethod
-    public void connect(JSObject argument, DoricPromise promise) {
-        
+    public void open(JSObject argument, DoricPromise promise) {
+        String fileName = argument.getProperty("fileName").asString().value();
+        DoricSQLiteDatabase database = new DoricSQLiteDatabase(getDoricContext().getContext().getDatabasePath(fileName));
+        String dbId = String.valueOf(atomicInteger.addAndGet(1));
+        sqLiteDatabaseMap.put(dbId, database);
+        promise.resolve(new JavaValue(dbId));
+    }
+
+    @DoricMethod
+    public void close(JSObject argument, DoricPromise promise) {
+        String dbId = argument.getProperty("dbId").asString().value();
+        DoricSQLiteDatabase database = sqLiteDatabaseMap.remove(dbId);
+        if (database != null) {
+            database.close();
+        }
+        promise.resolve();
+    }
+
+    @DoricMethod
+    public void execute(JSObject argument, DoricPromise promise) {
+        String dbId = argument.getProperty("dbId").asString().value();
+        String statement = argument.getProperty("statement").asString().value();
+        JSArray parameters = argument.getProperty("parameters").asArray();
+        DoricSQLiteDatabase database = sqLiteDatabaseMap.get(dbId);
+        if (database != null) {
+            database.execute(statement, parameters.toArray());
+            promise.resolve();
+        } else {
+            promise.reject(new JavaValue("Cannot find db for " + dbId));
+        }
+    }
+
+    @DoricMethod
+    public void executeQuery(JSObject argument, DoricPromise promise) {
+        String dbId = argument.getProperty("dbId").asString().value();
+        String statement = argument.getProperty("statement").asString().value();
+        JSArray parameters = argument.getProperty("parameters").asArray();
+        DoricSQLiteDatabase database = sqLiteDatabaseMap.get(dbId);
+        if (database != null) {
+            JSONArray jsonArray = database.executeQuery(statement, parameters.toArray());
+            promise.resolve(new JavaValue(jsonArray));
+        } else {
+            promise.reject(new JavaValue("Cannot find db for " + dbId));
+        }
+    }
+
+    @Override
+    public void onTearDown() {
+        super.onTearDown();
+        for (DoricSQLiteDatabase database : sqLiteDatabaseMap.values()) {
+            database.close();
+        }
+        sqLiteDatabaseMap.clear();
     }
 }
--- src/SQLite.ts
+++ src/SQLite.ts
@@ -2,8 +2,43 @@ import { BridgeContext } from "doric";
 
 export class Database {
   context: BridgeContext;
-  constructor(context: BridgeContext, fileName: string) {
+  dbId: string;
+  private constructor(context: BridgeContext, dbId: string) {
     this.context = context;
-    this.context.callNative("sqlite", "connect");
+    this.dbId = dbId;
+  }
+
+  public async execute(
+    statement: string,
+    parameters?: (string | number | null)[]
+  ) {
+    await this.context.callNative("sqlite", "execute", {
+      statement,
+      parameters,
+      dbId: this.dbId,
+    });
+  }
+  public async executeQuery(
+    statement: string,
+    parameters?: (string | number | null)[]
+  ) {
+    return await this.context.callNative("sqlite", "executeQuery", {
+      statement,
+      parameters: parameters || [],
+      dbId: this.dbId,
+    });
+  }
+
+  public async close() {
+    await this.context.callNative("sqlite", "close", {
+      dbId: this.dbId,
+    });
+  }
+
+  public static async open(context: BridgeContext, fileName: string) {
+    const dbId = (await context.callNative("sqlite", "open", {
+      fileName,
+    })) as string;
+    return new Database(context, dbId);
   }
 }
